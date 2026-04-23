ef520460df5cb5adc605448ad4cdb22b415122af
Infuseting <infuseting@gmail.com>
2026-04-23 21:30:24 +0200

    feat: basic bot bootstrap

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Client, GatewayIntentBits, Events } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
